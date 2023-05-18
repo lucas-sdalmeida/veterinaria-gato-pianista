1c60b28305ecf0d65ac2549d725520dcdd5f9c08
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-05-18 00:19:27 -0300

    feat: create the value object class CPF with multiton pattern

---
+++ model/util/CPF.php
@@ -0,0 +1,22 @@
+<?php
+    final class CPF {
+        private static array $instances = [];
+        private readonly string $cpfNumber;
+
+        private function __construct(string $cpfNumber) {
+            $this->cpfNumber = $cpfNumber;
+        }
+
+        private function of(string $cpfNumber) : CPF {
+            if (!static::isValidCPFNumber($cpfNumber))
+                throw new DomainException("The value <$cpfNumber> is not a valid CPF!");
+            if (!key_exists($cpfNumber, static::$instances));
+                self::$instances[$cpfNumber] = new CPF($cpfNumber);
+            return self::$instances[$cpfNumber];
+        }
+
+        private static function isValidCPFNumber(string $cpfNumber) : bool {
+            return preg_match('/^\d{3}(.?\d{3})-?\d{2}$/', $cpfNumber) > 0;
+        }
+    }
+?>
