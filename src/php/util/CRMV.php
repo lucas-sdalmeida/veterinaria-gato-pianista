e9b363f446f4b67a6db1272a23e5b55adaaf10ce
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-05-26 20:54:12 -0300

    feat: created CRMV value object class

---
+++ src/php/util/CRMV.php
@@ -0,0 +1,30 @@
+<?php
+    namespace php\util;
+
+use DomainException;
+
+    final class CRMV {
+        private static array $instances = [];
+        private readonly string $inscriptionCode;
+
+        private function __construct(string $inscriptionCode) {
+            $this->inscriptionCode = $inscriptionCode;
+        }
+
+        public static function of(string $inscriptionCode) : CRMV {
+            if (!static::isValidCRMV($inscriptionCode))
+                throw new DomainException("This is not a valid CRMV inscription number!");
+            if (!array_key_exists($inscriptionCode, static::$instances))
+                static::$instances[$inscriptionCode] = new CRMV($inscriptionCode);
+            return static::$instances[$inscriptionCode];
+        }
+
+        public static function isValidCRMV(string $inscriptionCode) {
+            return preg_match('/^CRMV-[A-Z]{2}\d+$/', $inscriptionCode) > 0;
+        }
+
+        public final function getInscriptionCode() : string {
+            return $this->inscriptionCode;
+        }
+    }
+?>
