5f57cd0d2a11246742a5f386edfb7729a6f835c6
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-06-03 00:28:59 -0300

    feat: implemented ConnectionFactory for MySQL using singleton, created a personal .json file containing database access information which is ignored by .gitignore

---
+++ src/model/repositories/mysql/SingletonMySQLConnectionFactory.php
@@ -0,0 +1,27 @@
+<?php
+    namespace pw2s3\clinicaveterinaria\model\repository\mysql;
+
+    use pw2s3\clinicaveterinaria\persistence\util\ConnectionFactory;
+    use pw2s3\clinicaveterinaria\model\repository\util\DBAccessDataProvider;
+    use PDO;
+
+    class SingletonMySQLConnectionFactory extends ConnectionFactory {
+        private const PATH = __DIR__ . "/../../resources/mysql-db-access-data.json";
+        private const PDO_OPTIONS = [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                                      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                                      PDO::ATTR_EMULATE_PREPARES => false
+                                    ];
+
+        private static ?PDO $connection = null;
+
+        public function getConnection() : PDO {
+            if ($this->connection == null){
+                $access = DBAccessDataProvider::initialize(self::PATH);
+                $this->connection = new PDO($access->getURL(), $access->getUsername(), 
+                                            $access->getPassword(), self::PDO_OPTIONS);
+            }
+
+            return $this->connection;
+        }
+    }
+?>
