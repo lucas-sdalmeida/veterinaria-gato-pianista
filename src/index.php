2f3ac7e2710b8bb8380d3175cfd46dd105d94285
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-06-05 22:04:19 -0300

    feat: cleaned index.php tests

--- src/index.php
+++ src/index.php
@@ -1,15 +1,7 @@
 <?php
     require_once(__DIR__ . "/../vendor/autoload.php");
 
-    use pw2s3\clinicaveterinaria\model\request\JSONRequestReader;
-    
-    header("Content-Type: application/json");
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE");
+    use pw2s3\clinicaveterinaria\model\repository\mysql\DatabaseBuilder;
 
-    $reader = new JSONRequestReader();
-
-    $request = $reader->readRequest();
-
-    echo json_encode($request->getAllParameters());
+    DatabaseBuilder::build();
 ?>
