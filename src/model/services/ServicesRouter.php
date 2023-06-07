7aa988a4891cfb77dcea7d3cb370bbce6612e6ae
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-06-06 23:39:28 -0300

    feat: created a TutorService class and routed it

--- src/model/services/ServicesRouter.php
+++ src/model/services/ServicesRouter.php
@@ -4,14 +4,24 @@
     use pw2s3\clinicaveterinaria\model\request\Request;
     use pw2s3\clinicaveterinaria\model\router\Route;
     use pw2s3\clinicaveterinaria\model\router\Router;
+    use pw2s3\clinicaveterinaria\model\services\tutor\TutorService;
 
     final class ServicesRouter implements Router {
-        private const SERVICES_ROUTES = [
-            
-        ];
+        private static array $SERVICES_ROUTES = [];
+
+        public function __construct() {
+            static::initializeRoutes();
+        }
+
+        private static function initializeRoutes() : void {
+            if (count(static::$SERVICES_ROUTES) > 0)
+                return;
+                
+            static::$SERVICES_ROUTES[] = new TutorService();
+        }
 
         public function findRouteByRequest(Request $request): Route {
-            foreach(self::SERVICES_ROUTES as $service)
+            foreach(static::$SERVICES_ROUTES as $service)
                 if ($service->isRoute($request->getURI())) return $service;
             return new ErrorRoute();
         }
---
+++ src/model/services/tutor/TutorService.php
@@ -0,0 +1,39 @@
+<?php
+    namespace pw2s3\clinicaveterinaria\model\services\tutor;
+
+    use pw2s3\clinicaveterinaria\model\request\HTTPUtils;
+    use pw2s3\clinicaveterinaria\model\request\Request;
+    use pw2s3\clinicaveterinaria\model\request\Response;
+    use pw2s3\clinicaveterinaria\model\request\HTTPMethod;
+    use pw2s3\clinicaveterinaria\model\router\Route;
+
+    final class TutorService implements Route {
+        private const SERVICE_PATH_REGEX = '/veterinaria-gato-pianista\/tutor\/?/';
+        private static array $METHOD_HANDLERS = [];
+
+        public function __construct() {
+            static::initializeRoutes();
+        }
+
+        private static function initializeRoutes() : void {
+            if (count(static::$METHOD_HANDLERS) > 0)
+                return;
+            
+            static::$METHOD_HANDLERS[HTTPMethod::GET->value] = new GetTutorMethodHandler();
+        }
+
+        public function redirectRequest(Request $request): Response {
+            if (!array_key_exists($request->getMethod()->value, static::$METHOD_HANDLERS))
+                return HTTPUtils::generateErrorReponse(400, "Unknown method <" . $request->getMethod()->value . ">!");
+
+            $requestReader = new TutorRequestReader();
+            $request = $requestReader->upgradeRequest($request);
+
+            return static::$METHOD_HANDLERS[$request->getMethod()->value]->handle($request);
+        }
+
+        public function isRoute(string $path) : bool {
+            return preg_match(self::SERVICE_PATH_REGEX, $path) > 0;
+        }
+    }
+?>
