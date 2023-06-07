60dcd4f2314ac608060c44ce447c8e06a33ae6f8
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-06-07 01:18:24 -0300

    refactor: removed Validator and Notification in order to simplify the project and facilitate professor's life

--- src/model/services/util/Validator.php
+++
@@ -1,7 +0,0 @@
-<?php
-    namespace pw2s3\clinicaveterinaria\model\services\util;
-
-    abstract class Validator {
-        public abstract function validate(mixed $target) : Notification;
-    }
-?>
