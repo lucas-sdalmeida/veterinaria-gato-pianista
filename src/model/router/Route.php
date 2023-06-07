0edd419559749e0af7f88df4307fbf5d18a994e4
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-06-06 22:07:16 -0300

    refactor: replaced Route::follow() by Route::redirectRequest(Request )

--- src/model/router/Route.php
+++ src/model/router/Route.php
@@ -1,10 +1,11 @@
 <?php
     namespace pw2s3\clinicaveterinaria\model\router;
 
+    use pw2s3\clinicaveterinaria\model\request\Request;
     use pw2s3\clinicaveterinaria\model\request\Response;
 
     interface Route {
-        function follow() : Response;
+        function redirectRequest(Request $request) : Response;
 
         function isRoute(string $path) : bool;
     }
