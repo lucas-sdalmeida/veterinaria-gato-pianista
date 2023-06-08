8b4d4e2c87ff0f805a22c3d4e85de9bfe0a8a2be
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-06-08 20:53:17 -0300

    refactor: moved ErrorRoute.php from src/model/services/ to src/model/application

--- src/model/application/ErrorRoute.php
+++ src/model/application/ErrorRoute.php
@@ -1,12 +1,12 @@
 <?php
-    namespace pw2s3\clinicaveterinaria\model\services;
+    namespace pw2s3\clinicaveterinaria\model\application;
 
-use pw2s3\clinicaveterinaria\model\request\Request;
-use pw2s3\clinicaveterinaria\model\request\Response;
+    use pw2s3\clinicaveterinaria\model\request\Request;
+    use pw2s3\clinicaveterinaria\model\request\Response;
     use pw2s3\clinicaveterinaria\model\router\Route;
 
     final class ErrorRoute implements Route {
-        private const RIGHT_ROUTE_PATH = '/veterinaria-gato-pianista\/[^(tutor|animal|doctor|appointment).*]$/';
+        private const RIGHT_ROUTE_PATH = '/veterinaria-gato-pianista\/[^(tutor|animal|doctor|appointment|login).*]$/';
 
         public function redirectRequest(Request $request) : Response {
             $response = new Response(404);
