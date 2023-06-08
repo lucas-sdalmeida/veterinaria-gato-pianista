2a9ada5045cff981da64c6c8c5b8991aa4ee08d4
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-06-08 20:55:15 -0300

    feat: now ServicesRouter.php is also a route of MainRouter.php

--- src/model/services/ServicesRouter.php
+++ src/model/services/ServicesRouter.php
@@ -2,11 +2,14 @@
     namespace pw2s3\clinicaveterinaria\model\services;
 
     use pw2s3\clinicaveterinaria\model\request\Request;
+    use pw2s3\clinicaveterinaria\model\request\Response;
     use pw2s3\clinicaveterinaria\model\router\Route;
     use pw2s3\clinicaveterinaria\model\router\Router;
     use pw2s3\clinicaveterinaria\model\services\tutor\TutorService;
+    use pw2s3\clinicaveterinaria\model\application\ErrorRoute;
 
-    final class ServicesRouter implements Router {
+    final class ServicesRouter implements Router, Route {
+        private const ROUTE_PATH_REGEX = '/veterinaria-gato-pianista\/(account|tutor|animal|doctor|appointment)\/?/';
         private static array $SERVICES_ROUTES = [];
 
         public function __construct() {
@@ -24,6 +27,16 @@
             foreach(static::$SERVICES_ROUTES as $service)
                 if ($service->isRoute($request->getURI())) return $service;
             return new ErrorRoute();
+        }
+
+        public function redirectRequest(Request $request): Response {
+            $serviceRoute = $this->findRouteByRequest($request);
+
+            return $serviceRoute->redirectRequest($request);
+        }
+
+        public function isRoute(string $path): bool {
+            return preg_match(self::ROUTE_PATH_REGEX, $path) > 0;
         }
     }
 ?>
