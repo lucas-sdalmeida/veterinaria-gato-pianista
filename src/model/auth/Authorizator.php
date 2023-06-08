9094028320f165a4a2082c6a6219f937c91f9f9f
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-06-08 17:43:08 -0300

    style: indented the use statement present in Authorizator.php

--- src/model/auth/Authorizator.php
+++ src/model/auth/Authorizator.php
@@ -1,7 +1,7 @@
 <?php
     namespace pw2s3\clinicaveterinaria\model\auth;
 
-use pw2s3\clinicaveterinaria\model\request\Request;
+    use pw2s3\clinicaveterinaria\model\request\Request;
 
     interface Authorizator {
         function createSession(Request $request) : Session;
