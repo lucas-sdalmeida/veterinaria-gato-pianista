29341f5fe3914e98c13d86089047ddea16fd1c9b
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-06-08 21:03:06 -0300

    feat: implemented MainRouter in index.php

--- src/index.php
+++ src/index.php
@@ -1,14 +1,19 @@
 <?php
     require_once(__DIR__ . "/../vendor/autoload.php");
 
+    use pw2s3\clinicaveterinaria\model\application\MainRouter;
     use pw2s3\clinicaveterinaria\model\request\JSONRequestReader;
     use pw2s3\clinicaveterinaria\model\request\JSONResponseSender;
-    use pw2s3\clinicaveterinaria\model\services\ServicesRouter;
+
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Access-Control-Allow-Origin");
 
     $requestReader = new JSONRequestReader();
     $request = $requestReader->readRequest();
 
-    $router = new ServicesRouter();
+    $router = new MainRouter();
     $route = $router->findRouteByRequest($request);
     
     $response = $route->redirectRequest($request);
