bc578daa117407759e1531cc947e7d6f9df7b4f6
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-06-08 21:37:09 -0300

    feat: added the session argument to ErrorRoute method

--- src/model/application/ErrorRoute.php
+++ src/model/application/ErrorRoute.php
@@ -1,6 +1,7 @@
 <?php
     namespace pw2s3\clinicaveterinaria\model\application;
 
+    use pw2s3\clinicaveterinaria\model\auth\Session;
     use pw2s3\clinicaveterinaria\model\request\Request;
     use pw2s3\clinicaveterinaria\model\request\Response;
     use pw2s3\clinicaveterinaria\model\router\Route;
@@ -8,7 +9,7 @@
     final class ErrorRoute implements Route {
         private const RIGHT_ROUTE_PATH = '/veterinaria-gato-pianista\/[^(tutor|animal|doctor|appointment|login).*]$/';
 
-        public function redirectRequest(Request $request) : Response {
+        public function redirectRequest(Request $request, ?Session $session = null) : Response {
             $response = new Response(404);
             $response->addContent("cause", "There is not such path '" . $request->getURI() . "'!");
 
